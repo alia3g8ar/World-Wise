222753bb9f19bec376fdc5f4d7ec16ac228394e5
alia3g8ar <alia3g8ar@gmail.com>
2024-12-31 19:02:56 +0330

    [ADD] React Router [ADD] Pages [ADD] Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Product from "./pages/Product.jsx"
+import Pricing from "./pages/Pricing.jsx"
+import HomePage from "./pages/HomePage.jsx"
+import PageNotFound from "./pages/PageNotFound.jsx"
+
 function App() {
-  return <h1>World Wise alia3g8ar</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
