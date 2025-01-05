9c304b6c46b64f98c1bef715d5ff2a31d7fbf6dd
alia3g8ar <alia3g8ar@gmail.com>
2025-01-05 18:14:19 +0330

    [ADD] Nested Routes and Index Route [Update] AppNav

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,14 @@ import styles from "./Sidebar.module.css"
 import Logo from "../Logo/Logo.jsx"
 import AppNav from "../AppNav/AppNav.jsx"
 import Footer from "../Footer/Footer.jsx"
+import { Outlet } from "react-router-dom"
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
-      <p>list of cities</p>
+      <Outlet />
 
       <Footer />
     </div>
