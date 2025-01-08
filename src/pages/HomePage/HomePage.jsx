e3d3ef7f1694c551f727916c721e45d1fca49f51
alia3g8ar <alia3g8ar@gmail.com>
2025-01-08 07:10:47 +0330

    [ADD] SplitText animation

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,29 @@
 import styles from "./Homepage.module.css"
 import { Link } from "react-router-dom"
 import PageNav from "../../components/PageNav/PageNav.jsx"
+import SplitText from "../../components/SplitText/SplitText.jsx"
 
 export default function Homepage() {
+  const handleAnimationComplete = () => {
+    console.log("All letters have animated!")
+  }
+
   return (
     <main className={styles.homepage}>
       <PageNav />
       <section>
         <h1>
-          You travel the world.
+          <SplitText
+            text="You travel the world."
+            className="text-2xl font-semibold text-center"
+            delay={150}
+            animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+            animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+            easing="easeOutCubic"
+            threshold={0.2}
+            rootMargin="-50px"
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
           <br />
           WorldWise keeps track of your adventures.
         </h1>
