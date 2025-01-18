51bec4c16ba9fcbd18839dac7a438611cce44081
alia3g8ar <alia3g8ar@gmail.com>
2025-01-18 16:39:14 +0330

    [Update] Marker

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,15 +3,13 @@ import { useNavigate, useSearchParams } from "react-router-dom"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import { useState } from "react"
 import { useCities } from "../../contexts/Cities.jsx"
-import city from "../City/City.jsx"
+
 function Map() {
   const navigate = useNavigate()
   const { cities } = useCities()
   const [mapPosition, setMapPosition] = useState([40, 0])
 
   const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get("lat")
-  const lng = searchParams.get("lng")
 
   return (
     <div className={styles.mapContainer}>
